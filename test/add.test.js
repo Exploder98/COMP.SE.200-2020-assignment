b4692892aa33828e850a0dcec8ed8c960ee8eaf7
Esko Toivonen <esko.toivonen@tuni.fi>
2020-11-27 17:04:00 +0200

    Tehty add-funktion yksikkötestit

--- test/add.test.js
+++ test/add.test.js
@@ -1,8 +1,55 @@
 /* jshint esversion: 6 */
+
+/**
+ * add-funktion yksikkötestit
+ *
+ * @group unit
+ */
+
 import add from "../src/add.js";
 
 
-test("Lisää 1 ja 1", () => {
-    expect(add(1, 1))
-        .toBe(2);
+const MAX_INTEGER = 1.7976931348623157e+308;
+
+
+describe("unit/add", () => {
+    it("1 + 1 = 2", () => {
+        expect(add(1, 1))
+            .toBe(2);
+    });
+
+    it("-1 + 1 = 0", () => {
+        expect(add(-1, 1))
+            .toBe(0);
+    });
+
+    it("1 + -1 = 0", () => {
+        expect(add(1, -1))
+            .toBe(0);
+    });
+
+    it("0 + 1 = 1", () => {
+        expect(add(0, 1))
+            .toBe(1);
+    });
+
+    it("1 + 0 = 1", () => {
+        expect(add(1, 0))
+            .toBe(1);
+    });
+
+    it("MAX_INTEGER + 0 = MAX_INTEGER", () => {
+        expect(add(MAX_INTEGER, 0))
+            .toBe(MAX_INTEGER);
+    });
+
+    it("0 + MAX_INTEGER = MAX_INTEGER", () => {
+        expect(add(0, MAX_INTEGER))
+            .toBe(MAX_INTEGER);
+    });
+
+    it("MAX_INTEGER + MAX_INTEGER = Infinity", () => {
+        expect(add(MAX_INTEGER, MAX_INTEGER))
+            .toBe(Infinity);
+    });
 });
