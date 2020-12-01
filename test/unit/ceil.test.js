d749e887031379d56da5ceac4fa31e14211a9d6a
Eerik Voimanen <eerik.voimanen@tuni.fi>
2020-12-01 18:41:31 +0200

    Lisätty ceil:n yksikkötestit

---
+++ test/unit/ceil.test.js
@@ -0,0 +1,51 @@
+/* jshint esversion: 6 */
+
+/**
+ * ceil-funktion yksikkötestit
+ *
+ * @group unit
+ */
+
+import ceil from "../../src/ceil.js";
+
+describe("unit/ceil", () => {
+    it("1, -> 1", () => {
+        expect(ceil(1,))
+            .toStrictEqual(1);
+    });
+
+    it("4.006, -> 5", () => {
+        expect(ceil(4.006,))
+            .toStrictEqual(5);
+    });
+
+    it("6.004, 2 -> 6.01", () => {
+        expect(ceil(6.004,2))
+            .toStrictEqual(6.01);
+    });
+
+    it("6040, -2 -> 6100", () => {
+        expect(ceil(6040,-2))
+            .toStrictEqual(6100);
+    });
+
+    it("-1.2, -> -1", () => {
+        expect(ceil(-1.2,))
+            .toStrictEqual(-1);
+    });
+
+    it("undefined, -> NaN", () => {
+        expect(ceil(undefined,))
+            .toStrictEqual(NaN);
+    });
+
+    it("null, -> NaN", () => {
+        expect(ceil(null,))
+            .toStrictEqual(NaN);
+    });
+
+    it("NaN, -> NaN", () => {
+        expect(ceil(NaN,))
+            .toStrictEqual(NaN);
+    });
+});
