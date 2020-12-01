7271fb389dbb32e3dc3121c1a41751a81a7eac76
Eerik Voimanen <eerik.voimanen@tuni.fi>
2020-12-01 18:31:22 +0200

    Lisätty yksikkötesti castArraylle

---
+++ test/unit/castArray.test.js
@@ -0,0 +1,51 @@
+/* jshint esversion: 6 */
+
+/**
+ * castArray-funktion yksikkötestit
+ *
+ * @group unit
+ */
+
+import castArray from "../../src/castArray.js";
+
+describe("unit/castArray", () => {
+    it("(tyhjä) -> []", () => {
+        expect(castArray())
+            .toStrictEqual([]);
+    });
+
+    it("1, 2, 3 -> [1, 2, 3]", () => {
+        expect(castArray(1, 2, 3))
+            .toStrictEqual([1, 2, 3]);
+    });
+
+    it("1 -> [1]", () => {
+        expect(castArray(1))
+            .toStrictEqual([1]);
+    });
+
+    it("{\"a\": 1} -> [{\"a\": 1}]", () => {
+        expect(castArray({"a": 1}))
+            .toStrictEqual([{"a": 1}]);
+    });
+
+    it("undefined -> [undefined]", () => {
+        expect(castArray(undefined))
+            .toStrictEqual([undefined]);
+    });
+
+    it("null -> [null]", () => {
+        expect(castArray(null))
+            .toStrictEqual([null]);
+    });
+
+    it("\"abc\" -> [\"abc\"]", () => {
+        expect(castArray("abc"))
+            .toStrictEqual(["abc"]);
+    });
+
+    it("[1, 2, 3] -> [1, 2, 3]", () => {
+        expect(castArray([1, 2, 3]))
+            .toStrictEqual([1, 2, 3]);
+    });
+});
