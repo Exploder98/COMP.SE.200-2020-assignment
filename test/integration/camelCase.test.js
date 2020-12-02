0cab0232c9f58f793a69d024c557b21f24f708f4
Esko Toivonen <esko.toivonen@tuni.fi>
2020-12-02 19:22:18 +0200

    [skip ci] Korjattu lainausmerkit

--- test/integration/camelCase.test.js
+++ test/integration/camelCase.test.js
@@ -10,31 +10,31 @@ import camelCase from "../../src/camelCase.js";
 
 
 describe("integration/camelCase", () => {
-    it("“oHjElMisToN mEsTaUs” -> “ohjelmistonMestaus”", () => {
+    it("\"oHjElMisToN mEsTaUs\" -> \"ohjelmistonMestaus\"", () => {
         expect(camelCase("oHjElMisToN mEsTaUs"))
             .toBe("ohjelmistonMestaus");
     });
-    it("“__FEELS_BATMAN???” -> “feelsBatman”", () => {
+    it("\"__FEELS_BATMAN???\" -> \"feelsBatman\"", () => {
         expect(camelCase("__FEELS_BATMAN???"))
             .toBe("feelsBatman");
     });
-    it("“+-joku-_-randomi” -> “jokuRandomi”", () => {
+    it("\"+-joku-_-randomi\" -> \"jokuRandomi\"", () => {
         expect(camelCase("+-joku-_-randomi"))
             .toBe("jokuRandomi");
     });
-    it("“42” -> “42”", () => {
+    it("\"42\" -> \"42\"", () => {
         expect(camelCase("42"))
             .toBe("42");
     });
-    it("NaN -> “nan”", () => {
+    it("NaN -> \"nan\"", () => {
         expect(camelCase(NaN))
             .toBe("nan");
     });
-    it("undefined -> “undefined”", () => {
+    it("undefined -> \"undefined\"", () => {
         expect(camelCase(undefined))
             .toBe("undefined");
     });
-    it("null -> “null”", () => {
+    it("null -> \"null\"", () => {
         expect(camelCase(null))
             .toBe("null");
     });
