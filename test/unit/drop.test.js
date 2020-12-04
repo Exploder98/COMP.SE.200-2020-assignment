a9b7ccddea065c21d66943c415778c79a8e66a3c
Esko Toivonen <esko.toivonen@tuni.fi>
2020-12-04 20:35:07 +0200

    Tehty dropin yksikkötestit

---
+++ test/unit/drop.test.js
@@ -0,0 +1,82 @@
+/**
+ * drop-funktion yksikkötestit
+ *
+ * @group unit
+ */
+
+import drop from "../../src/drop.js";
+import slice from "../../src/slice.js";
+import toInteger from "../../src/toInteger.js";
+
+
+jest.mock("../../src/slice.js");
+jest.mock("../../src/toInteger.js");
+
+
+describe("unit/drop", () => {
+
+    slice.mockImplementation((array, start, end) => {
+        if (start > array.length) {
+            return [];
+        }
+        const result = new Array(end - start);
+        let index = -1;
+        while (++index < (end - start)) {
+            result[index] = array[start + index];
+        }
+        return result;
+    });
+
+    it("Oletusdroppausmäärä (n=1) (dokumentaatioesimerkki)", () => {
+        toInteger.mockReturnValueOnce(1);
+        expect(drop([1, 2, 3]))
+            .toStrictEqual([2, 3]);
+    });
+
+    it("Dropataan 2 alkiota (dokumentaatioesimerkki)", () => {
+        toInteger.mockReturnValueOnce(2);
+        expect(drop([1, 2, 3], 2))
+            .toStrictEqual([3]);
+    });
+
+    it("Dropataan enemmän alkioita kuin listassa on (dokumentaatioesimerkki)", () => {
+        toInteger.mockReturnValueOnce(5);
+        expect(drop([1, 2, 3], 5))
+            .toStrictEqual([]);
+    });
+
+    it("Dropataan 0 alkiota (dokumentaatioesimerkki)", () => {
+        toInteger.mockReturnValueOnce(0);
+        expect(drop([1, 2, 3], 0))
+            .toStrictEqual([1, 2, 3]);
+    });
+
+    it("Dropataan NaN alkiota", () => {
+        toInteger.mockReturnValueOnce(0);
+        expect(drop([1, 2, 3], NaN))
+            .toStrictEqual([1, 2, 3]);
+    });
+
+    it("Dropataan Infinity alkiota", () => {
+        toInteger.mockReturnValueOnce(Infinity);
+        expect(drop([1, 2, 3], Infinity))
+            .toStrictEqual([]);
+    });
+
+    it("Dropataan -1 alkiota", () => {
+        expect(drop([1, 2, 3], -1))
+            .toStrictEqual([1, 2, 3]);
+    });
+
+    it("Dropataan \"two\" alkiota", () => {
+        toInteger.mockReturnValueOnce(2);
+        expect(drop([1, 2, 3], "two"))
+            .toStrictEqual([3]);
+    });
+
+    it("Dropataan alkioita objektista", () => {
+        toInteger.mockReturnValueOnce(2);
+        expect(drop({}, 2))
+            .toStrictEqual([]);
+    });
+});
