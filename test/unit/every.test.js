a4d8543f255ef9dde240d56fd5fac3a3cf0bf85c
Eerik Voimanen <eerik.voimanen@tuni.fi>
2020-12-05 14:24:20 +0200

    Lisätty yksikkötesti everylle

---
+++ test/unit/every.test.js
@@ -0,0 +1,55 @@
+/**
+ * every-funktion yksikkötestit
+ *
+ * @group unit
+ */
+
+import every from "../../src/every.js";
+
+
+describe("unit/every", () => {
+    it("[true, 1, null, \"yes\"], Boolean-> false", () => {
+        expect(every([true, 1, null, "yes"],Boolean))
+            .toBe(false);
+    });
+    
+    it("[] -> true", () => {
+        expect(every([]))
+            .toBe(true);
+    });
+    
+    it("[true], Boolean -> true", () => {
+        expect(every([true], Boolean))
+            .toBe(true);
+    });
+    
+    it("[true], () => {return false} -> false", () => {
+        expect(every([true], () => {return false}))
+            .toBe(false);
+    });
+    
+    it("[false], () => {return true} -> true", () => {
+        expect(every([false], () => {return true}))
+            .toBe(true);
+    });
+    
+    it("[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], Boolean-> false", () => {
+        expect(every([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], Boolean))
+            .toBe(false);
+    });
+    
+    it("[42], Boolean -> true", () => {
+        expect(every([42], Boolean))
+            .toBe(true);
+    });
+    
+    it("null, null -> false", () => {
+        expect(every(null, null))
+            .toBe(false);
+    });
+    
+    it("undefined, undefined -> false", () => {
+        expect(every(undefined, undefined))
+            .toBe(false);
+    });
+});
