5b74c6ce21f00f4c360640b6bb14c19ec0d9cc68
Eerik Voimanen <eerik.voimanen@tuni.fi>
2020-12-06 16:01:46 +0200

    Lisätty words yksikkötesti

---
+++ test/unit/words.test.js
@@ -0,0 +1,50 @@
+/**
+ * words-funktion yksikkötestit
+ *
+ * @group unit
+ */
+
+import words from "../../src/words.js";
+
+
+describe("unit/words", () => {
+    it("\"\" -> []", () => {
+        expect(words(""))
+            .toStrictEqual([]);
+    });
+
+    it("\"bruh moment\" -> [\"bruh\", \"moment\"]", () => {
+        expect(words("bruh moment"))
+            .toStrictEqual(["bruh", "moment"]);
+    });
+
+    it("\"Eerik, & Esko\" -> [\"Eerik\", \"Esko\"]", () => {
+        expect(words("Eerik, & Esko"))
+            .toStrictEqual(["Eerik", "Esko"]);
+    });
+
+    it("\"Eerik, & Esko\", /[^, ]+/g -> [\"Eerik\", \"&\", \"Esko\"]", () => {
+        expect(words("Eerik, & Esko",/[^, ]+/g))
+            .toStrictEqual(["Eerik", "&", "Esko"]);
+    });
+
+    it("null -> []", () => {
+        expect(words(null))
+            .toStrictEqual([]);
+    });
+
+    it("undefined -> []", () => {
+        expect(words(undefined))
+            .toStrictEqual([]);
+    });
+
+    it("NaN -> []", () => {
+        expect(words(NaN))
+            .toStrictEqual([]);
+    });
+
+    it("3.2 -> \"3.2\"", () => {
+        expect(words(3.2))
+            .toStrictEqual("3.2");
+    });
+});
